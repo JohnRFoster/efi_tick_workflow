f56916189df3ee2a16b9dba2819b596274785092
JohnRFoster <fosterj@bu.edu>
2022-04-02 12:46:50 -0400

    month intercept, comments

--- 2_stateSpaceModel.R
+++ 2_stateSpaceModel.R
@@ -2,38 +2,46 @@ library(nimble)
 
 model.code <- nimbleCode({
   
+  tau.proc ~ dexp(1)  # process error prior
+  
+  # regression coefficient priors
+  for (i in 1:2) {
+    beta[i] ~ dnorm(0, tau = 1 / 3 ^ 2) # standard deviation of 3
+  }
+  
+  for(i in 1:n.months){
+    alpha.month[i] ~ dnorm(0, tau = 1)
+  }
+  
   for (s in 1:n.sites) {
     
-    # priors
-    for (i in 1:3) {
-      # regression coefficients 
-      beta[s, i] ~ dnorm(0, tau = 1 / 3 ^ 2) # standard deviation of 3
-    }
-    tau.proc[s] ~ dinvgamma(0.1, 0.1) # process error
-    tau.obs[s] ~ dinvgamma(0.1, 0.1)  # observation error
+    tau.obs[s] ~ dexp(1)   # observation error prior
     
-    # first latent state at each site
-    x[s, first.drag[s]] ~ dexp(1)
+    # first latent state at each site prior
+    x[s, first.drag[s]] ~ dnorm(10, 2)
     
     # process model
     for (t in first.drag[s]:(last.drag - 1)) {
       
       # error in variables for temp and rh
-      # observed ~ N(true, error)
+      # observed ~ N(true, observed error)
       temp.o[s, t] ~ dnorm(temp[s, t], var = temp.var[s, t])
       rh.o[s, t] ~ dnorm(rh[s, t], var = rh.var[s, t])
       
       # priors for true weather, which has been centered and scaled
-      temp[s, t] ~ dnorm(0, tau = 1)
+      temp[s, t] ~ dnorm(0, tau = 1) 
       rh[s, t] ~ dnorm(0, tau = 1)
       
       # population growth a function of temperature and relative humidity
-      lambda[s, t] <- beta[s, 1] + 
-        beta[s, 2] * temp[s, t] +
-        beta[s, 3] * rh[s, t]
+      # with month specific intercepts
+      lambda[s, t] <- alpha.month[month[t]] + 
+        beta[1] * temp[s, t] +
+        beta[2] * rh[s, t] 
+        
       
-      ex[s, t] <- x[s, t] * lambda[s, t]
-      x[s, t+1] ~ dnorm(ex[s, t], tau = tau.proc[s])
+      ex[s, t] <- x[s, t] * lambda[s, t]        # expected number of ticks 
+      m[s, t] ~ dnorm(ex[s, t], tau = tau.proc) # process error
+      x[s, t+1] <- max(0, m[s, t])              # can't be negative! 
     }  
     
     # data model
@@ -43,3 +51,5 @@ model.code <- nimbleCode({
     
   }
 }) 
+
+
